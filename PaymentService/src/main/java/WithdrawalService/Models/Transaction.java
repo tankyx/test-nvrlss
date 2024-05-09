5e0e5fb2cdc7a8144c1ed44ef1cf57fe5cea71cf
tankyx <tanguy.pedrazzoli@gmail.com>
2024-05-09 11:40:54 +0200

    Updates to models
    Added getters/setters, hashCode, isEquals and toString to models

--- PaymentService/src/main/java/WithdrawalService/Models/Transaction.java
+++ PaymentService/src/main/java/WithdrawalService/Models/Transaction.java
@@ -1,6 +1,8 @@
 package WithdrawalService.Models;
 
 import java.math.BigDecimal;
+import java.util.Objects;
+
 import WithdrawalService.WithdrawalService.WithdrawalState;
 
 /*
@@ -19,4 +21,76 @@ public class Transaction {
     String receiverId;
     BigDecimal amount;
     WithdrawalState state;
+
+    public Transaction(String id, String senderId, String receiverId, BigDecimal amount, WithdrawalState state) {
+        this.id = id;
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.amount = amount;
+        this.state = state;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(String receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public WithdrawalState getState() {
+        return state;
+    }
+
+    public void setState(WithdrawalState state) {
+        this.state = state;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return Objects.equals(id, that.id) && Objects.equals(senderId, that.senderId) && Objects.equals(receiverId, that.receiverId) && Objects.equals(amount, that.amount) && state == that.state;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, senderId, receiverId, amount, state);
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id='" + id + '\'' +
+                ", senderId='" + senderId + '\'' +
+                ", receiverId='" + receiverId + '\'' +
+                ", amount=" + amount +
+                ", state=" + state +
+                '}';
+    }
 }
--- PaymentService/src/main/java/WithdrawalService/Models/User.java
+++ PaymentService/src/main/java/WithdrawalService/Models/User.java
@@ -1,18 +1,93 @@
 package WithdrawalService.Models;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 /*
     * User model
 
     * id - user id
+    * address - user address
     * name - username
     * surname - user surname
     * balance - user balance
  */
 public class User {
     String id;
+    String address;
     String name;
     String surname;
     BigDecimal balance;
+
+    public User(String id, String address, String name, String surname, BigDecimal balance) {
+        this.id = id;
+        this.address = address;
+        this.name = name;
+        this.surname = surname;
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(address, user.address) && Objects.equals(name, user.name) && Objects.equals(surname, user.surname) && Objects.equals(balance, user.balance);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, address, name, surname, balance);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", address='" + address + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
