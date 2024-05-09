f7558395e17a122b1ece286c25455b94683f10b1
tankyx <tanguy.pedrazzoli@gmail.com>
2024-05-09 11:42:00 +0200

    Creation of basic inMemory Data Storages
    Can add/get/remove user, add/get transaction, update transaction state

--- PaymentService/src/main/java/WithdrawalService/DataStore/InMemory.java
+++ PaymentService/src/main/java/WithdrawalService/DataStore/InMemory.java
@@ -1,4 +1,42 @@
 package WithdrawalService.DataStore;
 
+import WithdrawalService.Models.Transaction;
+import WithdrawalService.Models.User;
+import WithdrawalService.WithdrawalService.WithdrawalState;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 public class InMemory {
+    private final ConcurrentMap<String, User> users = new ConcurrentHashMap<>();
+    private final ConcurrentMap<String, Transaction> transactions = new ConcurrentHashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user.getId(), user);
+    }
+
+    public User getUser(String id) {
+        return users.get(id);
+    }
+
+    public boolean removeUser(String id) {
+        return users.remove(id) != null;
+    }
+
+    public void addTransaction(Transaction transaction) {
+        transactions.putIfAbsent(transaction.getId(), transaction);
+    }
+
+    public Transaction getTransaction(String id) {
+        return transactions.get(id);
+    }
+
+    public boolean changeTransactionState(String id, WithdrawalState state) {
+        Transaction transaction = transactions.get(id);
+        if (transaction == null) {
+            return false;
+        }
+        transaction.setState(state);
+        return true;
+    }
 }
