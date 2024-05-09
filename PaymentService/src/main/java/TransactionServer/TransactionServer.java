9f88903d881c37dbd91710c38aaf0dea407ac916
tankyx <tanguy.pedrazzoli@gmail.com>
2024-05-09 13:34:46 +0200

    Added main class to run the GRPC server

--- PaymentService/src/main/java/TransactionServer/TransactionServer.java
+++ PaymentService/src/main/java/TransactionServer/TransactionServer.java
@@ -1,4 +1,17 @@
 package TransactionServer;
 
+import TransactionService.TransactionServiceImpl;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
 public class TransactionServer {
+    public static void main(String[] args) throws Exception {
+        Server server = ServerBuilder.forPort(50051)
+                .addService(new TransactionServiceImpl())
+                .build()
+                .start();
+
+        System.out.println("Server started on port 50051");
+        server.awaitTermination();
+    }
 }
