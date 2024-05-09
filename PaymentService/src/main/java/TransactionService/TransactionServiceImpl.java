3ba8bb68de36681e873838bc6f00f901664c7317
tankyx <tanguy.pedrazzoli@gmail.com>
2024-05-09 13:35:48 +0200

    Implemented AddUser, Internal and External transactions

--- PaymentService/src/main/java/TransactionService/TransactionServiceImpl.java
+++ PaymentService/src/main/java/TransactionService/TransactionServiceImpl.java
@@ -1,13 +1,17 @@
 package TransactionService;
 
 import WithdrawalService.DataStore.InMemory;
+import WithdrawalService.Models.Transaction;
+import WithdrawalService.Models.User;
 import WithdrawalService.WithdrawalService;
 import WithdrawalService.WithdrawalServiceStub;
+import WithdrawalService.WithdrawalService.WithdrawalState;
 import com.TransactionProcess.grpc.*;
 import io.grpc.stub.StreamObserver;
 
-import java.util.ArrayList;
+import java.math.BigDecimal;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -22,16 +26,119 @@ public class TransactionServiceImpl extends TransactionServiceGrpc.TransactionSe
 
     @Override
     public void addUser(AddUserRequest request, StreamObserver<AddUserResponse> responseObserver) {
+        AddUserResponse response = null;
+
+        if (memory.userExists(request.getId())) {
+            response = AddUserResponse.newBuilder()
+                    .setUserId(request.getId())
+                    .setStatus("ERROR : User already exists")
+                    .build();
+        } else {
+            memory.addUser(request.getName(), request.getSurname(), request.getId(), request.getBalance());
+            response = AddUserResponse.newBuilder()
+                    .setUserId(request.getId())
+                    .setStatus("SUCCESS")
+                    .build();
+        }
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
 
     }
     @Override
     public void sendMoneyInternally(InternalTransferRequest request, StreamObserver<TransactionResponse> responseObserver) {
+        User sender = memory.getUser(request.getSenderId());
+        User recipient = memory.getUser(request.getRecipientId());
+        String transactionId = "TX" + transactionCounter.getAndIncrement();
+
+        //Both users exists
+        if (sender != null && recipient != null) {
+            BigDecimal transactionAmount = BigDecimal.valueOf(request.getAmount());
+            if (!memory.createTransaction(transactionId, sender.getId(), recipient.getId(), transactionAmount)) {
+                TransactionResponse response = TransactionResponse.newBuilder()
+                        .setTransactionId(transactionId)
+                        .setStatus("Failed to create transaction : Transaction already exists.")
+                        .build();
+            }
+
+            // If the sender balance is not sufficient, set the transaction state to FAILED
+            // Else, update the balances of both clients and set the transaction state to COMPLETED
+            if (sender.getBalance().compareTo(transactionAmount) < 0) {
+                memory.changeTransactionState(transactionId, WithdrawalState.FAILED);
+            } else {
+                sender.setBalance(sender.getBalance().subtract(transactionAmount));
+                recipient.setBalance(recipient.getBalance().add(transactionAmount));
+                memory.changeTransactionState(transactionId, WithdrawalState.COMPLETED);
+            }
+
+            WithdrawalState status = memory.getTransaction(transactionId).getState();
+            notifyObservers(transactionId, status.toString());
+
+            TransactionResponse response = TransactionResponse.newBuilder()
+                    .setTransactionId(transactionId)
+                    .setStatus(status.toString())
+                    .build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } else {
+            if (!memory.userExists(request.getSenderId())) {
+                TransactionResponse response = TransactionResponse.newBuilder()
+                        .setTransactionId(transactionId)
+                        .setStatus("FAILED : Sender ID doesn't exist")
+                        .build();
+                responseObserver.onNext(response);
+                responseObserver.onCompleted();
+                return;
+            }
+            if (!memory.userExists(request.getRecipientId())) {
+                TransactionResponse response = TransactionResponse.newBuilder()
+                        .setTransactionId(transactionId)
+                        .setStatus("FAILED : Recipient ID doesn't exist")
+                        .build();
+                responseObserver.onNext(response);
+                responseObserver.onCompleted();
+            }
+        }
 
     }
 
     @Override
     public void sendMoneyExternally(ExternalWithdrawalRequest request, StreamObserver<TransactionResponse> responseObserver) {
+        String transactionId = "TX" + transactionCounter.getAndIncrement();
 
+        if (!memory.userExists(request.getUserId())) {
+            TransactionResponse response = TransactionResponse.newBuilder()
+                    .setTransactionId(transactionId)
+                    .setStatus("FAILED : Sender ID doesn't exist")
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+            return;
+        }
+
+        User sender = memory.getUser(request.getUserId());
+        BigDecimal transactionAmount = BigDecimal.valueOf(request.getAmount());
+
+        memory.createTransaction(transactionId, sender.getId(), request.getExternalAddress(), transactionAmount);
+
+        // If the sender balance is not sufficient, set the transaction state to FAILED
+        if (sender.getBalance().compareTo(transactionAmount) < 0) {
+            memory.getTransaction(transactionId).setState(WithdrawalState.FAILED);
+        } else {
+            memory.getTransaction(transactionId).setState(WithdrawalState.PROCESSING);
+            sender.setBalance(sender.getBalance().subtract(transactionAmount));
+            withdrawalService.requestWithdrawal(new WithdrawalService.WithdrawalId(UUID.randomUUID()),
+                    new WithdrawalService.Address(request.getExternalAddress()), BigDecimal.valueOf(request.getAmount()));
+        }
+        WithdrawalState status = memory.getTransaction(transactionId).getState();
+        notifyObservers(transactionId, status.toString());
+
+        TransactionResponse response = TransactionResponse.newBuilder()
+                .setTransactionId(transactionId)
+                .setStatus(status.toString())
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 
     @Override
